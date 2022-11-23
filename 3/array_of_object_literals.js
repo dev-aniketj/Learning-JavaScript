02becb291ab6a73b0aa84d07c083d720f7b497ef
Aniket Jain <81229551+dev-aniketj@users.noreply.github.com>
2022-11-23 17:01:25 +0530

    added 3,4 folders

---
+++ 3/array_of_object_literals.js
@@ -0,0 +1,26 @@
+let person = [
+  {
+    name: "aniket",
+    age: 23,
+    language: "react js",
+  },
+  {
+    name: "abhishek",
+    age: 21,
+    language: "node js",
+  },
+  {
+    name: "aditi",
+    age: 23,
+    language: "c#",
+  },
+];
+
+console.log(person);
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+
+console.log(person[0].name);
+console.log(person[0]["age"]);
+console.log(person[0].language);
